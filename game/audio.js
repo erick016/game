ff76175bd7ff65675fe8d6e62df51e4294bfbd25
james <erick016@cougars.csusm.edu>
2013-09-04 19:19:27 -0700

    Audio attempt

---
+++ game/audio.js
@@ -0,0 +1,46 @@
+var audio = (function () {
+
+helloWorld = function()
+{
+	console.log("Hello world.");
+}
+
+
+var context;
+
+init = function () {
+  try {
+    // Fix up for prefixing
+    window.AudioContext = window.AudioContext||window.webkitAudioContext;
+    context = new AudioContext();
+  }
+  catch(e) {
+    alert('Web Audio API is not supported in this browser');
+  }
+}
+
+window.addEventListener('load', init, false);
+
+loadSound = function (url) {
+  var request = new XMLHttpRequest();
+  request.open('GET', url, true);
+  request.responseType = 'arraybuffer';
+
+  // Decode asynchronously
+  request.onload = function() {
+    context.decodeAudioData(request.response, function(buffer) {
+      thisSoundBuffer = buffer;
+    }, onError);
+  }
+  request.send();
+	return thisSoundBuffer;
+}
+
+playSound = function (buffer) {
+  var source = context.createBufferSource(); // creates a sound source
+  source.buffer = buffer;                    // tell the source which sound to play
+  source.connect(context.destination);       // connect the source to the context's destination (the speakers)
+  source.start(0);                           // play the source now
+                                             // note: on older systems, may have to use deprecated noteOn(time);
+}
+})();
